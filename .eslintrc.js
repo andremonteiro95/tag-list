3e7b84f2614e61035cdea1d99f1354ef9eecea7e
andremonteiro95 <andremonteiro95@gmail.com>
2022-01-06 13:19:21 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
